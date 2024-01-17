7c40d1622f42930d0484bf91dee585f1e8cad6e5
unknown <atout.maris@gmail.com>
2024-01-17 23:22:23 +0100

    Adding bootstrap and testing it in src/app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logoNoir from './assets/logoNoir.png';
 function App() {
   return (
-    <div className="App">
-      App works !
+  <>
+      <div className="App">
+      <div className='card text-center' >
+        <img src={logoNoir}  className="card-img-top" alt="ResumeBuilder logo"/>
+        <h1 className="card-title">Welcome to ResumeBuilder</h1>
+      </div>
     </div>
+  </>
   );
 }
 
