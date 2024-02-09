792a6244c3ce7fe4a287cb86e64288e185aa20c2
ouss223 <oussama.ghazouani@insat.ucar.tn>
2024-02-09 09:01:10 +0100

    editing page added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import index from './index.css';
 import logoNoir from './assets/logoNoir.png';
+import EditingPage from './components/EditingPage';
+
 function App() {
   return (
   <>
-      <div className="App">
-      <div className='card text-center' >
-        <img src={logoNoir}  className="card-img-top" alt="ResumeBuilder logo"/>
-        <h1 className="card-title">Welcome to ResumeBuilder</h1>
-      </div>
-    </div>
+     <EditingPage/>
   </>
   );
 }
+
+
+/*
+<div className="App">
+      <div className='card text-center h-screen flex flex-col items-center justify-center ' >
+
+        <div className='bg-blue-200 rounded-xl p-4 flex flex-col items-center justify-center shadow-lg'>
+        <img src={logoNoir}  className="rounded h-[200px] w-[200px] " alt="ResumeBuilder logo"/>
+        <h1 className=" rounded-lg font-semibold text-lg mx-auto">Welcome to ResumeBuilder</h1>
+        </div>
+      </div>
+    </div>
+
+*/ 
 
 export default App;
